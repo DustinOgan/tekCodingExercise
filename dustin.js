de5539dcdff3203daca1c706ba5e374b6d06c9a8
Dustin Ogan <dustin.ogan@universalorlando.com>
2018-12-26 15:29:56 -0500

    initial commit

---
+++ dustin.js
@@ -0,0 +1,91 @@
+const request = require('superagent');
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+dustinMain();
+async function dustinMain() {
+    let stateMapByName = new Map();
+    const response = await request.get('http://services.groupkt.com/state/get/USA/all');
+    let stateArray = await response.body.RestResponse.result;
+    let stateMaps = await fillStateMaps(stateArray);
+
+    rl.setPrompt('Please Enter a State or State Code For Lookup:>  ');
+    rl.clearLine();
+    rl.prompt();
+
+    rl.on('line', function (line) {
+        switch (line.trim()) {
+            case 'hello':
+                console.log('world!');
+                break;
+            default:
+                let returnedStates;
+                let stateRequested = line.trim();
+                if(isValid(stateRequested, stateMaps)){
+                    console.log('You entered `' + stateRequested + '`');
+                    if(stateRequested.length == 2){
+                         returnedStates = stateMaps.abbrMap;
+                    }else{
+                         returnedStates = stateMaps.nameMap;
+                    }
+                    console.log('The state capital for queried state is  : ' + returnedStates.get(stateRequested).capital);
+                    console.log('The largest city for the queried state is : ' + returnedStates.get(stateRequested).largest_city) ;
+
+                }
+                break;
+        }
+        rl.prompt();
+    }).on('close', function () {
+        console.log('You are now exiting the state lookup program');
+        process.exit(0);
+    });
+
+}
+
+function fillStateMaps(sArray) {
+    let sMapByAbbr = new Map();
+    let sMapByName = new Map();
+    sArray.forEach(function (state) {
+        sMapByAbbr.set(state.abbr.toLowerCase(), { 'capital': state.capital, 'largest_city': state.largest_city });
+        sMapByName.set(state.name.toLowerCase(), { 'capital': state.capital, 'largest_city': state.largest_city });
+    });
+    return { 'nameMap': sMapByName, 'abbrMap': sMapByAbbr };
+};
+
+function isValid(input, stateMaps) {
+    var hasNumber = /\d/;
+    if (hasNumber.test(input)) {
+        console.log('This request contains numbers, please try a different lookup');
+        return false;
+    };
+    if (input.length < 2) {
+        console.log('This request is not long enought to query for a state');
+        return false;
+    };
+    if (input.length == 2) {
+        if (!stateMaps.abbrMap.has(input)) {
+            console.log('This 2 letter code was not found amongs the valid state abbreviations list');
+            console.log('Please try a different state code');
+            return false;
+        }
+    };
+    if (input.length > 2) {
+        if (!stateMaps.nameMap.has(input)) {
+            console.log('This name code was not found in the valid state names list');
+            console.log('Please try a different state name');
+            return false;
+        }
+    };
+    return true;
+
+
+}
+
+
+module.exports = {
+    fillStateMaps,
+    isValid
+}
---
+++ test/dustin-test.spec.js
@@ -0,0 +1,76 @@
+const isValid = require('../dustin.js').isValid;
+const fillStateMaps = require('../dustin.js').fillStateMaps;
+var expect = require('chakram').expect;
+
+
+describe ('The state lookup program', function(){
+    let stateMaps; 
+    var stateArray = [
+        {
+        id: 1,
+        country: "USA",
+        name: "Alabama",
+        abbr: "AL",
+        area: "135767SKM",
+        largest_city: "Birmingham",
+        capital: "Montgomery"
+        },
+        {
+        id: 2,
+        country: "USA",
+        name: "Alaska",
+        abbr: "AK",
+        area: "1723337SKM",
+        largest_city: "Anchorage",
+        capital: "Juneau"
+        },
+        {
+        id: 3,
+        country: "USA",
+        name: "Arizona",
+        abbr: "AZ",
+        area: "113594SKM",
+        largest_city: "Phoenix",
+        capital: "Phoenix"
+        }]
+        
+    before(function(){
+        stateMaps = fillStateMaps(stateArray)
+    });
+
+    describe('for method isValid ', function(){
+
+        it('should return false for data less than 2 characters', function(){
+            expect(isValid('i', null)).to.be.false;
+        });
+        it('should return false for numeric data ', function() {
+            expect(isValid('1', null)).to.be.false;
+        });
+        it('should return false when an otherwise valid 2 letter code is not found', function(){
+            expect(isValid('zo',stateMaps)).to.be.false;
+        });
+        it('should return false when an otherwise valid 2 letter code is not found', function(){
+            expect(isValid('florida',stateMaps)).to.be.false;
+        });
+
+        it('should return true for known valid input found in the state names map', function(){
+            expect(isValid('alabama',stateMaps)).to.be.true;
+        })
+        it('should return true for known valid input found in the state abbreviation map', function(){
+            expect(isValid('al',stateMaps)).to.be.true;
+        })
+
+    });
+
+    describe('for method fillStateMaps', function() {
+        
+        it('should build a searchable abbreviation lookup containing Alabama', function(){
+           expect(stateMaps.abbrMap.get('al').capital).to.equal('Montgomery');
+        });
+
+        it('should build a searchable name lookup map containing Alabama', function(){
+            expect(stateMaps.nameMap.get('alabama').capital).to.equal('Montgomery');
+         });
+    });
+
+});
